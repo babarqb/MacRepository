875b56c33598409479b342437a062a993c93bdec
Babar QB <babarqb@babars-mac.local>
2016-03-28 10:16:50 +0500

    Initial Commit

---
+++ XCodeAssets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XCodeAssets
+//
+//  Created by Babar QB on 28/03/2016.
+//  Copyright © 2016 Babar QB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
